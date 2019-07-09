957594f1317889fb48ab8bf9b2b0ab6b1b66a93d
Maxime Rouiller <angelzerosoft@gmail.com>
2019-07-09 15:24:29 -0400

    fixing the environment variable target

--- src/EasyAuthAuthenticationHandler.cs
+++ src/EasyAuthAuthenticationHandler.cs
@@ -26,7 +26,7 @@ namespace MaximeRouiller.Azure.AppService.EasyAuth
         {
             try
             {
-                bool easyAuthEnabled = String.Equals(Environment.GetEnvironmentVariable("WEBSITE_AUTH_ENABLED"), "True", StringComparison.InvariantCultureIgnoreCase);
+                bool easyAuthEnabled = String.Equals(Environment.GetEnvironmentVariable("WEBSITE_AUTH_ENABLED", EnvironmentVariableTarget.Process), "True", StringComparison.InvariantCultureIgnoreCase);
                 if (easyAuthEnabled) return Task.FromResult(AuthenticateResult.NoResult());
                 
                 string easyAuthProvider = Context.Request.Headers["X-MS-CLIENT-PRINCIPAL-IDP"].FirstOrDefault();
